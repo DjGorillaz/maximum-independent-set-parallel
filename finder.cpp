8e55fb315cdbd434961a537e506c1feb93c206fd
Gorillaz Dj <djg2008@gmail.com>
2018-09-16 03:34:21 +0300

    Add new interface for finders * "run2" function uses virtual functions from the derived classes * Added virtual public functions: get _time, _result, _name

--- finder.cpp
+++ finder.cpp
@@ -1,13 +1,46 @@
 #include "finder.h"
 
-#include "graph.h"
-
 namespace MaximumIndependentSet
 {
-    Finder::Finder(const Graph& graph_, int nCpu_):
-        nCpu(nCpu_),
+    Finder::Finder(const Graph& graph_, const std::string& name,  int nCpu_ ):
         graph(graph_),
+        nCpu(nCpu_),
         nVertices(graph_.nVertices),
-        graphB(graph.graphB)
+        graphB(graph.graphB),
+        algo_name(name)
     {    }
+
+    void Finder::run2()
+    {
+        using namespace std::chrono;
+        using namespace boost;
+
+        auto start_time = high_resolution_clock::now();
+
+        //todo: rename?
+        unsigned long long nTasks = get_nTasks();
+
+        unsigned long long perCpu = nTasks / nCpu;
+        unsigned long long l = 0;
+        unsigned long long r = perCpu - 1 + (nTasks % nCpu);
+    
+        //Parallelization of task
+        thread_group threadGroup;
+        while(l < nTasks)
+        {
+            thread* th = new thread(&Finder::find_per_thread, this, l, r);
+            threadGroup.add_thread(th);
+            l = r + 1;
+            r = r + perCpu;
+        }
+        threadGroup.join_all();
+
+        //todo: rename? find MIS
+        calc_result();
+
+        auto end_time = high_resolution_clock::now();
+        ms = duration_cast<milliseconds>(end_time-start_time);
+    }
+
+    
 }
--- finder.h
+++ finder.h
@@ -5,23 +5,33 @@
 #include <chrono>
 #include <boost/thread.hpp>
 
-
 namespace MaximumIndependentSet
 {
     class Finder
     {
     public:
-        Finder(const Graph& graph, int nCpu = 1);
-        virtual void run() = 0;
+        Finder(const Graph& graph, const std::string& name, int nCpu = 1);
         virtual decltype(std::chrono::milliseconds().count()) get_time() const = 0;
-        virtual void get_result() const = 0;
-        virtual void print_result() const = 0;
+        virtual std::vector<int> get_result() const = 0;
+        virtual std::string get_name() const = 0;
+       
+        void run2();
+
 
     protected:
+        virtual unsigned long long get_nTasks() const = 0;
+        virtual void find_per_thread(unsigned long long l, unsigned long long r) = 0;
+        virtual void calc_result() = 0;
+
         Graph graph;
         int nCpu = 0;
-        std::chrono::milliseconds ms;
-        int nVertices = 0;
+        int nVertices = 0; //Is needed?
         GraphBoost& graphB;
+        std::chrono::milliseconds ms {0};
+        std::vector<int> max_independent_set;
+        std::string algo_name;
+        int index_max = 0;
+        unsigned long long tasks = 0;
+
     };
 }
